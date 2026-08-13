9db0fbfe3d505b88659e267c887aaad2c8b1aeec
juaninho <molini1405@gmail.com>
2026-08-13 18:03:10 -0300

    Configurar PostgreSQL y dependencias

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
+import os
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+
+# =====================================================
+# CONFIGURACIÓN DE BASE DE DATOS
+# =====================================================
+
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url:
+    # Render puede entregar DATABASE_URL con postgres://
+    # SQLAlchemy utiliza postgresql://
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace(
+            "postgres://",
+            "postgresql://",
+            1
+        )
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+
+else:
+    # Mientras desarrollamos localmente
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        "postgresql://porti:porti123@localhost:5433/porti_db"
+    )
+
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db = SQLAlchemy(app)
 
 
 # =====================================================
@@ -31,7 +66,7 @@ def cumpleanos():
 
 
 # =====================================================
-# EJECUTAR APLICACIÓN
+# EJECUTAR
 # =====================================================
 
 if __name__ == "__main__":
